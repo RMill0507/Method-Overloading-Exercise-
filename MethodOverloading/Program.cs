4a3da0c8fd702ef7f2809049d01c86fe9c6a6eff
Rob <Rmilla445@gmail.com>
2022-10-25 12:23:05 -0400

    Method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodOverloading
 {
     public class Program
     {
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //var answer = Add(2, 2);
+            //Console.WriteLine(answer);
+            var answer = Add(3, 5, true);
+            {
+
+                Console.WriteLine(answer);
+            }
+
         }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x = y;
+        }
+        public static string Add(int x, int y, bool withDollars)
+
+        {
+            if (withDollars && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+
+            }
+            else if (withDollars && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x = y}";
+            }
+
+
+
+
+
+        }
+
+
     }
 }
